70ca4f6603c104b3075054416028bb44721efe47
cfchen01 <674441755@qq.com>
2019-10-23 18:17:19 +0800

    接口调整

---
+++ dfjinxin-population/population-server/src/main/java/com/dfjinxin/population/server/modules/dto/T04IndsWorkPersMemQtyChgTrndVo.java
@@ -0,0 +1,15 @@
+package com.dfjinxin.population.server.modules.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class T04IndsWorkPersMemStruChgTrndVo {
+
+    private List<String> desc;
+
+    private List<String> year;
+
+    private List<List<Integer>> lists;
+}
---
+++ dfjinxin-population/population-server/src/main/java/com/dfjinxin/population/server/modules/dto/T04IndsWorkPersMemStru5YrChgVo.java
@@ -0,0 +1,17 @@
+package com.dfjinxin.population.server.modules.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class T04IndsWorkPersMemStruVo {
+
+    private List<String> desc;
+
+    private List<String> year;
+
+    private List<List<String>> lists;
+
+
+}
